c221b62799f8f97543ccf4dfe5df21a3d08931f3
tonymushah <tonyramanantsalama@gmail.com>
2024-01-29 12:28:32 +0300

    Added `MenuPrefix`

---
+++ src/lib/styles/MenuPrefix.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let firstLevel = false;
+	export let collapsed = false;
+	export let rtl = false;
+	export let rootStyles: string | null = null;
+</script>
+
+<span style={rootStyles} class:transition={true} class:fc={firstLevel && collapsed} class:rtl>
+	<slot />
+</span>
+
+<style>
+	.rtl {
+		margin-left: 5px;
+	}
+	:not(.rtl) {
+		margin-right: 5px;
+	}
+	.fc {
+		opacity: 0;
+	}
+	:not(.fc) {
+		opacity: 1;
+	}
+	.transition {
+		opacity: opacity var(--transitionDuration) ms;
+	}
+</style>
