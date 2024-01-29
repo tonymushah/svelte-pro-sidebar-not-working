62701df0fe84c6bc523748eaf6644d3bc1009f30
tonymushah <tonyramanantsalama@gmail.com>
2024-01-29 11:34:54 +0300

    Added `ExpandIcon`

---
+++ src/lib/styles/ExpandIcon.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let open: boolean = false;
+	export let rtl: boolean = false;
+    export let style: string | null = null;
+</script>
+
+<span 
+    class:rtl={rtl}
+    class:open={open}
+    {style}
+>
+	<slot />
+</span>
+
+<style>
+	span {
+		display: inline-block;
+		transition: transform 0.3s;
+		width: 5px;
+		height: 5px;
+        transform: rotate('-45deg');
+	}
+    :not(.rtl){
+        border-right: 2px solid currentcolor;
+		border-bottom: 2px solid currentcolor;
+    }
+	.rtl {
+		border-left: 2px solid currentcolor;
+		border-top: 2px solid currentcolor;
+	}
+	.open.rtl  {
+		transform: rotate('-135deg');
+	}
+
+	.open:not(.rtl) {
+		transform: rotate('45deg');
+	}
+
+</style>
