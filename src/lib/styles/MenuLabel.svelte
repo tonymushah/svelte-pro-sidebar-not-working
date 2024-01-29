efd5c0cf7c5fbba67b200a195b03e8cfb9818e1f
tonymushah <tonyramanantsalama@gmail.com>
2024-01-29 12:10:47 +0300

    Added MenuLabel

---
+++ src/lib/styles/MenuLabel.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let rootStyles: string | null = null;
+</script>
+
+<span style={rootStyles}>
+	<slot />
+</span>
+
+<style>
+	span {
+		flex-grow: 1;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+</style>
