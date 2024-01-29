e9469a461525053e1768d81d605b3751d1920c05
tonymushah <tonyramanantsalama@gmail.com>
2024-01-29 10:50:15 +0300

    Added `Backdrop.svelte`

---
+++ src/lib/styles/Backdrop.svelte
@@ -0,0 +1,15 @@
+<div>
+	<slot />
+</div>
+
+<style>
+	div {
+		position: fixed;
+		top: 0px;
+		right: 0px;
+		bottom: 0px;
+		left: 0px;
+		z-index: 1;
+		background-color: rgb(0, 0, 0, 0.3);
+	}
+</style>
